8319667c902438d0d0cb74406ef6191ddc797661
Andreea <andreeafarcas2803@gmail.com>
2024-05-12 15:26:58 +0300

    created show-view page

--- src/app/shows/show/show.component.ts
+++ src/app/shows/show/show.component.ts
@@ -25,7 +25,7 @@ export class ShowComponent {
     vote_average: 6.875,
     vote_count: 52,
   };
-  public readonly imgBaseUrl = 'https://image.tmdb.org/t/p/original';
+  public readonly imgBaseUrl = 'https://image.tmdb.org/t/p/w1280';
 }
 
 export default ShowComponent;
