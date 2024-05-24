90697a7cb6b97df3cac065b56fb47f0167e16189
Mihai Sava <mihai.sava@accesa.eu>
2024-05-24 09:50:09 +0300

    landing page components

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ContactUsComponent,
+  FooterComponent,
+  HeroComponent,
+  HowItWorksComponent,
+  NavbarComponent,
+  QuestionsComponent,
+  TeamComponent,
+  TestimonialsComponent,
+} from './';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [
+    HowItWorksComponent,
+    TestimonialsComponent,
+    NavbarComponent,
+    HeroComponent,
+    QuestionsComponent,
+    TeamComponent,
+    ContactUsComponent,
+    FooterComponent,
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LandingPageComponent {}
 
