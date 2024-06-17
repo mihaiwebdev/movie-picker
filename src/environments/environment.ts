2ad0eef26e84700567c64ef8966464f7c396abe4
Mihai Sava <mihai.sava@accesa.eu>
2024-06-17 19:02:22 +0300

    replace env files on production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,7 @@
+import { baseEnv } from './baseEnv';
+
 export const environment = {
-  baseAppUrl: 'https://moviepicker.app/app',
-  tmdbApiUrl: 'https://api.themoviedb.org/3',
-  ipInfoToken: 'e13edabe81e868',
-  ipInfoUrl: 'https://ipinfo.io/json',
-  tmdbApiAccessToken:
-    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMmRjMDI0Mjc2MWU3NmVmZGJmZTI1OGRjYjdiZjlkMCIsInN1YiI6IjYzNGU3YTY1Mzg5ZGExMDA3OWU2NGE1YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GQ3OTma_bWgWATd-IWV7i_F-BXgBRRTEz8XhzVO9XcY',
-  firebaseConfig: {
-    apiKey: 'AIzaSyBDDbR0I5tjk4t3QEx5QqogBBswoH9Tbhc',
-    authDomain: 'movie-picker-81e11.firebaseapp.com',
-    projectId: 'movie-picker-81e11',
-    storageBucket: 'movie-picker-81e11.appspot.com',
-    messagingSenderId: '458754057875',
-    appId: '1:458754057875:web:31b804b976463872710f66',
-    measurementId: 'G-44KF71FLTP',
-  },
-  email: 'email',
-  accessToken: 'mp_access_token',
+  // baseAppUrl: 'https://moviepicker.app/app',
+  baseAppUrl: 'https://favorite-movie-app-q6ir.vercel.app/app',
+  ...baseEnv,
 };
