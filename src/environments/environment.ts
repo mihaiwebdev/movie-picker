fa1a9b9a231bcd801cc92e7c3e62cadaf1c27b58
Mihai Sava <mihai.sava@accesa.eu>
2024-07-01 11:01:43 +0300

    changed base app url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ import { baseEnv } from './baseEnv';
 
 export const environment = {
   // baseAppUrl: 'https://moviepicker.app/app',
-  baseAppUrl: 'https://movie-picker-81e11.firebaseapp.com/app',
+  baseAppUrl: 'https://movie-picker-81e11.web.app/app',
   ...baseEnv,
 };
