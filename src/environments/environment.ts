6bacd895a1eb34cbc209fb1bc18524771a7821b0
Mihai Sava <mihai.sava@accesa.eu>
2024-07-01 08:48:09 +0300

    changed base url, hosted on firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ import { baseEnv } from './baseEnv';
 
 export const environment = {
   // baseAppUrl: 'https://moviepicker.app/app',
-  baseAppUrl: 'https://favorite-movie-app-q6ir.vercel.app/app',
+  baseAppUrl: 'https://movie-picker-81e11.firebaseapp.com/app',
   ...baseEnv,
 };
