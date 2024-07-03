bdb1945fb09922cea91dfa89e6dedcfa1e6fdfcb
Mihai Sava <mihai.sava@accesa.eu>
2024-07-03 17:04:41 +0300

    removed base href

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,13 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
-import { routes } from './routes';
 import { tmdbApiAuthInterceptor } from './core';
-import { APP_BASE_HREF } from '@angular/common';
+import { routes } from './routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([tmdbApiAuthInterceptor])),
     provideAnimationsAsync(),
-    { provide: APP_BASE_HREF, useValue: '/app' },
   ],
 };
