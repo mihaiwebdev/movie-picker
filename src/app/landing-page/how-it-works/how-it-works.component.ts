d6df7078020a619dedef2a5dd940c74aca5972d2
Mihai Sava <mihai.sava@accesa.eu>
2024-07-04 14:40:31 +0300

    added action on button from how it works

--- src/app/landing-page/how-it-works/how-it-works.component.ts
+++ src/app/landing-page/how-it-works/how-it-works.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-how-it-works',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './how-it-works.component.html',
   styleUrl: './how-it-works.component.css',
 })
